7633f6e9d0179144a99afbaf06b8e8bcf6d766d8
BoloMasta <103572991+BoloMasta@users.noreply.github.com>
2023-03-03 12:42:35 +0100

    write router functions

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,50 @@ const express = require("express");
 
 const router = express.Router();
 
+const contactsFunctions = require("../../models/contacts");
+
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const contacts = await contactsFunctions.listContacts();
+  res.json(contacts);
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { contactId } = req.params;
+  const contact = await contactsFunctions.getContactById(contactId);
+  if (!contact) {
+    return res.status(404).json({ message: `Contact with id=${contactId} was not found.` });
+  }
+  res.json(contact);
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    return res.status(400).json({ message: "Missing required name field" });
+  }
+  const newContact = await contactsFunctions.addContact(req.body);
+  res.status(201).json(newContact);
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { contactId } = req.params;
+  const contact = await contactsFunctions.removeContact(contactId);
+  if (!contact) {
+    return res.status(404).json({ message: `Contact with id=${contactId} was not found.` });
+  }
+  res.status(200).json({ message: `Contact with id=${contactId} was deleted.` });
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { contactId } = req.params;
+  if (!req.body.name && !req.body.email && !req.body.phone) {
+    return res.status(400).json({ message: "Missing required field" });
+  }
+  const contact = await contactsFunctions.updateContact(contactId, req.body);
+  if (!contact) {
+    return res.status(404).json({ message: `Contact with id=${contactId} was not found.` });
+  }
+  res.status(200).json(contact);
 });
 
 module.exports = router;
