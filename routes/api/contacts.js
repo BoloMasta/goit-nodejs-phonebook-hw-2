6a80e68d4f2577082d8ead185d01e245ce0d67ff
BoloMasta <boleslawadamiec@gmail.com>
2023-03-12 23:09:47 +0100

    try catch

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -28,46 +28,64 @@ router.get("/", async (req, res, next) => {
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await contactsFunctions.getContactById(contactId);
-
-  checkContactId(contact, contactId, res);
-
-  res.json(contact);
+  try {
+    const { contactId } = req.params;
+    const contact = await contactsFunctions.getContactById(contactId);
+    checkContactId(contact, contactId, res);
+    res.json(contact);
+  } catch (error) {
+    next(error);
+    return res.status(500).json({ message: "Server error" });
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  const { error, value } = validateCreateContact(req.body);
-  if (error) {
-    return res.status(400).json({ message: error.message });
-  }
-  const newContact = await contactsFunctions.addContact(req.body);
+  try {
+    const { error, value } = validateCreateContact(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.message });
+    }
+    const newContact = await contactsFunctions.addContact(req.body);
 
-  res.status(201).json(newContact);
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+    return res.status(500).json({ message: "Server error" });
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = await contactsFunctions.removeContact(contactId);
+  try {
+    const { contactId } = req.params;
+    const contact = await contactsFunctions.removeContact(contactId);
 
-  checkContactId(contact, contactId, res);
+    checkContactId(contact, contactId, res);
 
-  res
-    .status(200)
-    .json({ message: `Contact with id=${contactId} was deleted.` });
+    res
+      .status(200)
+      .json({ message: `Contact with id=${contactId} was deleted.` });
+  } catch (error) {
+    next(error);
+    return res.status(500).json({ message: "Server error" });
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const { error, value } = validateUpdateContact(req.body);
-  if (error) {
-    return res.status(400).json({ message: error.message });
+  try {
+    const { contactId } = req.params;
+    const { error, value } = validateUpdateContact(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.message });
+    }
+    const contact = await contactsFunctions.updateContact(contactId, req.body);
+
+    checkContactId(contact, contactId, res);
+
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+    return res.status(500).json({ message: "Server error" });
   }
-  const contact = await contactsFunctions.updateContact(contactId, req.body);
-
-  checkContactId(contact, contactId, res);
-
-  res.status(200).json(contact);
 });
 
 module.exports = router;
